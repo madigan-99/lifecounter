6d43fc64df76d996ae473865aaa5aade7249f12e
YuYu Madigan <fymadigan@outlook.com>
2021-04-22 11:12:26 -0700

    Initial Commit

---
+++ lifecounter_swiftui/lifecounter_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  lifecounter_swiftuiApp.swift
+//  lifecounter_swiftui
+//
+//  Created by stlp on 4/22/21.
+//
+
+import SwiftUI
+
+@main
+struct lifecounter_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
